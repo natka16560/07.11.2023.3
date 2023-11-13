39236e03b4aaf10ef0b84f728b2e1fa37ba49f1e
daaahha <dashka2016zhitnikova@gmail.com>
2023-11-13 20:41:17 +1200

    +Navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Main from "./Pages/Main/Main";
 import Catalog from "./Pages/Catalog/Catalog";
@@ -7,13 +7,14 @@ import Product from "./Pages/Product/Product";
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/main" element={Main} />
-        <Route exact path="/catalog" element={Catalog} />
-        <Route exact path="/product" element={Product} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="*" element={<Main />} /> 
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,19 @@
 import { headerData } from "./Header/headerData";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate()
+
   return (
     <div className="header">
       <div className="left-section">Это текст в левой части</div>
       <div className="right-section">
-        {headerData.map((i, index) => (
-          <Link key={index} to={i.link}>
-            <button>{i.name}</button>
-          </Link>
+        {headerData.map((i) => (
+          <div onClick={() => navigate(`${i.link}`)}>{i.name}</div>
         ))}
       </div>
     </div>
   )
 }
-export default Header;
 
+export default Header;
